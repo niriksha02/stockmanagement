c652b1593f25b68f64b3be8af180f6448fe215ef
niriksha02 <nirikshashree02@gmail.com>
2020-11-10 19:47:53 +0530

    added foreign key

--- venv/src/stockmgmt/admin.py
+++ venv/src/stockmgmt/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .forms import StockCreateForm
 
 # Register your models here.
-from .models import Stock
+from .models import *
 class StockCreateAdmin(admin.ModelAdmin):
    list_display = ['category', 'item_name', 'quantity']
    form = StockCreateForm
@@ -10,3 +10,4 @@ class StockCreateAdmin(admin.ModelAdmin):
    search_fields = ['category', 'item_name']
 
 admin.site.register(Stock,StockCreateAdmin)
+admin.site.register(Category)
--- venv/src/stockmgmt/models.py
+++ venv/src/stockmgmt/models.py
@@ -6,9 +6,14 @@ category_choice = (
 		('Phone', 'Phone'),
 	)
 
+class Category(models.Model):
+	name = models.CharField(max_length=50, blank=True, null=True)
+	def __str__(self):
+		return self.name
+
 # Create your models here.
 class Stock(models.Model):
-	category = models.CharField(max_length=50, blank=True, null=True, choices=category_choice)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	item_name = models.CharField(max_length=50, blank=True, null=True)
 	quantity = models.IntegerField(default='0', blank=True, null=True)
 	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
