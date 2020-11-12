21890db48316c4e53063ddb944dc73fa2c3093aa
niriksha02 <nirikshashree02@gmail.com>
2020-11-12 14:25:38 +0530

    added issue and receive button

--- venv/src/stockmgmt/views.py
+++ venv/src/stockmgmt/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
+from .forms import *
 import csv
 from .models import *
-from .forms import StockCreateForm,StockSearchForm,StockUpdateForm
+
 
 # Create your views here.
 def home(request):
@@ -90,3 +91,46 @@ def stock_detail(request, pk):
 		"queryset": queryset,
 	}
 	return render(request, "stock_detail.html", context)
+
+def issue_items(request, pk):
+	queryset = Stock.objects.get(id=pk)
+	form = IssueForm(request.POST or None, instance=queryset)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.quantity -= instance.issue_quantity
+		#instance.issue_by = str(request.user)
+		messages.success(request, "Issued SUCCESSFULLY. " + str(instance.quantity) + " " + str(instance.item_name) + "s now left in Store")
+		instance.save()
+
+		return redirect('/stock_detail/'+str(instance.id))
+		# return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+		"title": 'Issue ' + str(queryset.item_name),
+		"queryset": queryset,
+		"form": form,
+		"username": 'Issue By: ' + str(request.user),
+	}
+	return render(request, "add_items.html", context)
+
+
+
+def receive_items(request, pk):
+	queryset = Stock.objects.get(id=pk)
+	form = ReceiveForm(request.POST or None, instance=queryset)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.quantity += instance.receive_quantity
+		instance.save()
+		messages.success(request, "Received SUCCESSFULLY. " + str(instance.quantity) + " " + str(instance.item_name)+"s now in Store")
+
+		return redirect('/stock_detail/'+str(instance.id))
+		# return HttpResponseRedirect(instance.get_absolute_url())
+	context = {
+			"title": 'Reaceive ' + str(queryset.item_name),
+			"instance": queryset,
+			"form": form,
+			"username": 'Receive By: ' + str(request.user),
+		}
+	return render(request, "add_items.html", context)
+
