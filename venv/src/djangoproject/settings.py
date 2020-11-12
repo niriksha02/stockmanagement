cf7aa494bfeac0ceff8123418c8dcd9c8f642e52
niriksha02 <nirikshashree02@gmail.com>
2020-11-12 16:44:07 +0530

    configured to mysql

--- venv/src/djangoproject/settings.py
+++ venv/src/djangoproject/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#    'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stockmgmt',
+        'USER': 'myusername',
+        'PASSWORD': 'Niri@2000',
     }
 }
 
